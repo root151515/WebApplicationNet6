f040577ef4d355e162f4f5f3519012d25bee6930
DESKTOP-K1IUH04\clud1 <Sanbylai@gmail.com>
2023-05-01 09:06:00 +0800

    新增SanbyController

---
+++ WebApplicationNet6/Controllers/SanbyController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationNet6.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SanbyController : ControllerBase
+    {
+        [HttpGet("GetSanbyData")]
+        public IActionResult GetSanbyData()
+        {
+            return Ok(123);
+        }
+    }
+}
